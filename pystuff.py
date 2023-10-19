f36768816909d9fa2a47ac9ea273bba92c8567d5
PixelGiant <138526453+PixelGiant777@users.noreply.github.com>
2023-10-18 21:58:39 -0400

    Add files via upload

---
+++ pystuff.py
@@ -0,0 +1,10 @@
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+age = age + 1
+nationality = input("What is your nationality?: ")
+
+print("Your nationality is "+nationality)
+
+print("Hello "+name)
+print("You are "+str(age)+" years old")
+
