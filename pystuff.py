813bf457540125f6d069beb9f2a87c3b23ddcfd1
PixelGiant <138526453+PixelGiant777@users.noreply.github.com>
2023-10-18 22:34:05 -0400

    Update pystuff.py

--- pystuff.py
+++ pystuff.py
@@ -1,10 +1,7 @@
+timezone = input("What time zone are you in?: ")
+country = input("What country are you in?: ")
 name = input("What is your name?: ")
 age = int(input("How old are you?: "))
-age = age + 1
 nationality = input("What is your nationality?: ")
-
-print("Your nationality is "+nationality)
-
-print("Hello "+name)
-print("You are "+str(age)+" years old")
-
+age = age + 1
+print("Hello " + name + " You are " + str(age) + " years old Your nationality is " + nationality + " Your timezone is " + timezone + " The country you currently are in is " + country)
